743150ceb6bff3109a038dfc90c9d9094c12cd01
Rajin <rajin@rajinshankar.com>
2019-05-12 00:44:56 -0400

    Added terrain to demo tileset

--- content/import.tsx
+++ content/import.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="import" tilewidth="8" tileheight="8" tilecount="240" columns="15">
- <image source="content.png" width="127" height="128"/>
+<tileset version="1.2" tiledversion="1.2.1" name="import" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="content.png" width="128" height="128"/>
+ <terraintypes>
+  <terrain name="Grass" tile="133"/>
+ </terraintypes>
+ <tile id="132" terrain=",,,0"/>
+ <tile id="133" terrain=",,0,0"/>
+ <tile id="134" terrain=",,0,"/>
+ <tile id="135" terrain=",0,0,0"/>
+ <tile id="136" terrain="0,,0,0"/>
+ <tile id="148" terrain=",0,,0"/>
+ <tile id="149" terrain="0,0,0,0"/>
+ <tile id="150" terrain="0,,0,"/>
+ <tile id="151" terrain="0,0,,0"/>
+ <tile id="152" terrain="0,0,0,"/>
+ <tile id="164" terrain=",0,,"/>
+ <tile id="165" terrain="0,0,,"/>
+ <tile id="166" terrain="0,,,"/>
 </tileset>
