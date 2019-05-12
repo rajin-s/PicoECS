1fed2446278fe4decd3cd1e02e841df72a324eb3
Rajin (Laptop) <rajin>
2019-05-12 17:06:15 -0400

    updated content

--- content/map/import.tsx
+++ content/map/import.tsx
@@ -1,20 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="import" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+<tileset version="1.2" tiledversion="1.2.3" name="import" tilewidth="8" tileheight="8" tilecount="256" columns="16">
  <image source="../content.png" width="128" height="128"/>
  <terraintypes>
-  <terrain name="Grass" tile="133"/>
+  <terrain name="Dirt" tile="193"/>
+  <terrain name="Grass" tile="215"/>
  </terraintypes>
- <tile id="132" terrain=",,,0"/>
- <tile id="133" terrain=",,0,0"/>
- <tile id="134" terrain=",,0,"/>
- <tile id="135" terrain=",0,0,0"/>
- <tile id="136" terrain="0,,0,0"/>
- <tile id="148" terrain=",0,,0"/>
- <tile id="149" terrain="0,0,0,0"/>
- <tile id="150" terrain="0,,0,"/>
- <tile id="151" terrain="0,0,,0"/>
- <tile id="152" terrain="0,0,0,"/>
- <tile id="164" terrain=",0,,"/>
- <tile id="165" terrain="0,0,,"/>
- <tile id="166" terrain="0,,,"/>
+ <tile id="192" terrain=",,,0"/>
+ <tile id="193" terrain=",,0,0"/>
+ <tile id="194" terrain=",,0,"/>
+ <tile id="195" terrain="0,0,0,"/>
+ <tile id="196" terrain="0,0,,0"/>
+ <tile id="197" terrain=",,,1"/>
+ <tile id="198" terrain=",,1,1"/>
+ <tile id="199" terrain=",,1,"/>
+ <tile id="208" terrain=",0,,0"/>
+ <tile id="209" terrain="0,0,0,0"/>
+ <tile id="210" terrain="0,,0,"/>
+ <tile id="211" terrain="0,,0,0"/>
+ <tile id="212" terrain=",0,0,0"/>
+ <tile id="213" terrain=",1,,1"/>
+ <tile id="214" terrain="1,1,1,1"/>
+ <tile id="215" terrain="1,,1,"/>
+ <tile id="224" terrain=",0,,"/>
+ <tile id="225" terrain="0,0,,"/>
+ <tile id="226" terrain="0,,,"/>
+ <tile id="227" terrain="1,1,1,"/>
+ <tile id="228" terrain="1,1,,1"/>
+ <tile id="229" terrain=",1,,"/>
+ <tile id="230" terrain="1,1,,"/>
+ <tile id="231" terrain="1,,,"/>
+ <tile id="243" terrain="1,,1,1"/>
+ <tile id="244" terrain=",1,1,1"/>
 </tileset>
